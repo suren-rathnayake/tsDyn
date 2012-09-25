8cb3a85c2c9486f1d3b75a8e5693725a9a2cc012
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2012-09-25 09:30:55 +0200

    predict: improvements

--- tsDyn/R/predict.R
+++ tsDyn/R/predict.R
@@ -30,7 +30,7 @@ predict.nlar <- function(object, newdata, n.ahead=1, type=c("naive", "MC", "boot
   pred_fun <- function(res){ 
     innov <- if(type=="naive") rep(0, n.ahead) else if (type=="MC") rnorm(n.ahead, mean=0, sd=sd) else sample(resid, size=n.ahead, replace=TRUE)
     for(i in n.used + (1:n.ahead)) {
-      res[i] <- tsDyn:::oneStep.setar(object, newdata = t(as.matrix(res[i - xrange])),
+      res[i] <- tsDyn:::oneStep(object, newdata = t(as.matrix(res[i - xrange])),
 			itime=(i - n.used), ...)
       res[i] <- res[i] + innov[i-n.used]
     }
@@ -38,12 +38,12 @@ predict.nlar <- function(object, newdata, n.ahead=1, type=c("naive", "MC", "boot
   }
 
   res <- replicate(nboot, pred_fun(res=res))
-  res_means <- rowMeans(res)
+  res_means <- rowMeans(res, na.rm=TRUE)
   pred <- res_means[n.used + 1:n.ahead]
 
 ## Compute SE if MC/boot
   if(type!="naive"){
-    SE <- t(apply(res[n.used + 1:n.ahead, ,drop=FALSE], 1 ,quantile, prob=c(1-ci, ci)))
+    SE <- t(apply(res[n.used + 1:n.ahead, ,drop=FALSE], 1 ,quantile, prob=c(1-ci, ci), na.rm=TRUE))
     SE <- ts(SE, start = tsp(newdata)[2] + deltat(newdata),
              frequency=frequency(newdata))
   }
@@ -64,27 +64,37 @@ predict.nlar <- function(object, newdata, n.ahead=1, type=c("naive", "MC", "boot
 if(FALSE){
 library(tsDyn)
 
-set <- setar(lynx, m=1)
+set <- setar(lynx, m=2)
 tsDyn:::predict.nlar(set, n.ahead=1)
 
-environment(predict2.nlar) <- environment(star)
+environment(predict.nlar) <- environment(star)
 environment(oneStep) <- environment(star)
 
 ## n-ahead: 1
 tsDyn:::predict.nlar(set, n.ahead=1)
-predict2.nlar(set, n.ahead=1, type="naive")
-predict2.nlar(set, n.ahead=1, type="MC")
-predict2.nlar(set, n.ahead=1, type="bootstrap")
+tsDyn:::predict.nlar(set, n.ahead=1, type="naive")
+tsDyn:::predict.nlar(set, n.ahead=1, type="MC")
+tsDyn:::predict.nlar(set, n.ahead=1, type="bootstrap")
 
 ## n-ahead: 2
 tsDyn:::predict.nlar(set, n.ahead=2)
-predict2.nlar(set, n.ahead=2, type="naive")
-predict2.nlar(set, n.ahead=2, type="MC", nboot=100)
-predict2.nlar(set, n.ahead=2, type="bootstrap", nboot=100)
+tsDyn:::predict.nlar(set, n.ahead=2, type="naive")
+tsDyn:::predict.nlar(set, n.ahead=2, type="MC", nboot=100)
+tsDyn:::predict.nlar(set, n.ahead=2, type="bootstrap", nboot=100)
 
 ## n-ahead: 5
 tsDyn:::predict.nlar(set, n.ahead=5)
-predict2.nlar(set, n.ahead=5, type="naive")
-predict2.nlar(set, n.ahead=5, type="MC", nboot=5)
+tsDyn:::predict.nlar(set, n.ahead=5, type="naive")
+tsDyn:::predict.nlar(set, n.ahead=5, type="MC", nboot=5)
 
+###### LSTAR
+mod.lst <- lstar(lynx, m=2, th=1300)
+
+predict(mod.lst, n.ahead=5)
+predict(mod.lst, n.ahead=5, type="MC", nboot=100)
+predict(mod.lst, n.ahead=5, type="bootstrap", nboot=100)
+
+###### AAR
+mod.aar <- aar(lynx, m=2)
+predict(object=mod.aar , n.ahead=5)
 }
