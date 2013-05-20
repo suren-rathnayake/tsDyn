1d25a1649a6be4c798ee2b691269f34feb09c23c
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2013-05-20 18:47:12 +0200

    change lines below 100 characters

--- tsDyn/R/setar.sim.R
+++ tsDyn/R/setar.sim.R
@@ -236,7 +236,8 @@ library(tsDyn)
 environment(setar.sim)<-environment(star)
 
 ##Simulation of a TAR with 1 threshold
-sim<-setar.sim(B=c(2.9,-0.4,-0.1,-1.5, 0.2,0.3),lag=2, type="simul", nthresh=1, Thresh=2, starting=c(2.8,2.2))$serie
+Bvals <- c(2.9,-0.4,-0.1,-1.5, 0.2,0.3)
+sim<-setar.sim(B=Bvals,lag=2, type="simul", nthresh=1, Thresh=2, starting=c(2.8,2.2))$serie
 mean(ifelse(sim>2,1,0))	#approximation of values over the threshold
 
 #check the result
