c392a7d7b90c85a28015137850795ded73a3ff80
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2018-01-05 19:03:58 +0100

    TVAR sim: changes mainly in doc

--- tsDyn/R/TVAR.sim.R
+++ tsDyn/R/TVAR.sim.R
@@ -1,4 +1,4 @@
-TVAR.gen2 <- function(B, n=200, lag=1, include = c("const", "trend","none", "both"),  
+TVAR.gen <- function(B, n=200, lag=1, include = c("const", "trend","none", "both"),  
                       Thresh, nthresh=1, thDelay=1,  thVar=NULL, mTh=1, 
                       starting=NULL, innov, 
                       trendStart=1,
@@ -30,15 +30,17 @@ TVAR.gen2 <- function(B, n=200, lag=1, include = c("const", "trend","none", "bot
   
   ## starting values check dims
   if(!is.null(starting)){
-    if(all(dim(as.matrix(starting))==c(p,k)))
-      y[seq_len(p),]<- as.matrix(starting)
-    else
+    if(!all(dim(as.matrix(starting))==c(p,k)))
       stop("Bad specification of starting values. Should have nrow = lag and ncol = number of variables. But is: ", dim(as.matrix(starting)), sep="")
   }
   
   # trans var
-  temp <- tsDyn:::TVAR_thresh(mTh=mTh, thDelay=thDelay, thVar=thVar, y=y, p=p) #Stored in misc.R
-  combin <- temp$combin
+  if (length(mTh) == 1) {
+    combin <- matrix(0, ncol = 1, nrow = k)
+    combin[mTh, ] <- 1
+  }  else {
+    combin <- matrix(mTh, ncol = 1, nrow = k)
+  }
     
   
   if(nthresh%in%c(1,2))
@@ -55,7 +57,7 @@ TVAR.gen2 <- function(B, n=200, lag=1, include = c("const", "trend","none", "bot
   }
   #if (include=="both"): correction useless
   
-  Bmat <- tsDyn:::myInsertCol(B, c=a ,0)
+  Bmat <- myInsertCol(B, c=a ,0)
   nparBmat <- p*k+2
   
   
@@ -81,10 +83,9 @@ TVAR.gen2 <- function(B, n=200, lag=1, include = c("const", "trend","none", "bot
   resb <- rbind(matrix(0,nrow=p, ncol=k), 
                 innov)	
   trend <- 1:(n+p) #   trend<-c(NA, 1:(T-1+add))
-  trend <- c(rep(0, p), trendStart+(0:(T-1)))
+  trend <- c(rep(0, p), trendStart+(0:(n+p-1)))
   
   ## MAIN loop:
-  
   if(nthresh==0){
     for(i in (p+1):(n+p)){
       Yb[i,]<-rowSums(cbind(B[,1], B[,2]*trend[i], B[,-c(1,2)]%*%matrix(t(Yb[i-c(1:p),]), ncol=1),resb[i,]))
@@ -135,214 +136,6 @@ TVAR.gen2 <- function(B, n=200, lag=1, include = c("const", "trend","none", "bot
   if(add.regime) res <- cbind(res, regime)
   return(res)
 }
-# 
-# TVAR.gen <- function(data,B,TVARobject, Thresh, nthresh=1, type=c("simul","boot", "check"), 
-#                      n=200, lag=1, include = c("const", "trend","none", "both"),  
-#                      thDelay=1,  thVar=NULL, mTh=1, starting=NULL, 
-#                      innov=rmnorm(n, varcov=varcov), varcov=diag(1,k), 
-#                      trendStart=1,
-#                      show.parMat=FALSE, round=FALSE, seed){
-#   
-#   ###check correct arguments
-#   if(!nthresh%in%c(0,1,2))
-#     stop("Arg nthresh should  be either 0, 1 or 2")
-#   if(!missing(n)&any(!missing(data), !missing(TVARobject)))
-#     stop("arg n should not be given with arg data or TVARobject")
-#   if(!missing(TVARobject)&any(!missing(Thresh), !missing(nthresh), !missing(lag), !missing(thDelay), !missing(mTh)))
-#     warning("When object TVARobject is given, only args 'type' and 'round' are relevant, others are not considered")
-#   if(!missing(data)&!missing(B))
-#     stop("You have to provide either B or y, but not both")
-#   
-#   type<-match.arg(type)
-#   include<-match.arg(include)
-#   
-#   ## Create some variables/parameters
-#   p <- switch(type, "boot"=if(!missing(TVARobject)) TVARobject$lag else lag, lag)
-#   ninc <- switch(include, "none"=0, "const"=1, "trend"=1, "both"=2)
-#   add <- switch(type, "simul"=p, 0)
-#   if(max(thDelay)>p)
-#     stop("Max of thDelay should be smaller or equal to the number of lags")
-#   
-#   
-#   ### possibility 1: only parameters matrix is given
-#   if(!missing(B)){
-#     if(type!="simul"){
-#       type<-"simul"
-#       warning("Type check or boot are only avalaible with pre specified data. The type simul was used")
-#     }
-#     nB<-nrow(B)
-#     ndig<-4
-#     esp<-p*nB+ninc 
-#     if(esp*(nthresh+1)!=ncol(B)){
-#       stop("Matrix B badly specified: expected ", esp*(nthresh+1), " elements ( (lag*K+ n inc)* (nthresh+1) ) but has ", ncol(B), "\n" )
-#     }
-#     y<-matrix(0,ncol=nB, nrow=n+add)
-#     if(!is.null(starting)){
-#       if(all(dim(as.matrix(starting))==c(p,nB)))
-#         y[seq_len(p),]<- as.matrix(starting)
-#       else
-#         stop("Bad specification of starting values. Should have nrow = lag and ncol = number of variables. But is: ", dim(as.matrix(starting)), sep="")
-#     }
-#     Bmat<-B
-#     temp<-TVAR_thresh(mTh=mTh,thDelay=thDelay,thVar=thVar,y=y, p=p) #Stored in misc.R
-#     trans<-temp$trans
-#     combin<-temp$combin
-#     
-#     if(nthresh==1){
-#       if(missing(Thresh))
-#         Thresh<-mean(trans)
-#       if(length(Thresh)!=1){
-#         warning("Please only one Thresh value if you choose nthresh=1. First one was chosen")
-#         Thresh<-Thresh[1]}
-#     } else  if(nthresh==2){
-#       if(missing(Thresh))
-#         Thresh<-quantile(trans, probs=c(0.25, 0.75))
-#       if(length(Thresh)!=2)
-#         stop("please give two Thresh values if you choose nthresh=2")
-#     }
-#     if(nthresh%in%c(1,2))
-#       Thresh<-round(Thresh,ndig)
-#     k <- nB 		#Number of variables
-#     T <- nrow(y) 		#Size of start sample
-#     T <- n 		#Size of start sample
-#   }
-# 
-#   ### possibility 2: only data is given: compute it with linear or selectSETAR
-#   else if(!missing(data)){
-#     if(nthresh==0){
-#       TVARobject<-lineVar(data, lag=p, include=include, model="VAR")
-#     } else{ 
-#       if(!missing(Thresh)){
-#         TVARobject<-TVAR(data, lag=p, include=include, nthresh=nthresh, plot=FALSE, trace=FALSE, gamma=Thresh)
-#       } else{
-#         TVARobject<-TVAR(data, lag=p, include=include, nthresh=nthresh, plot=FALSE, trace=FALSE)
-#       }
-#     }
-#   }
-#   ### possibility 3: setarobject is given by user (or by poss 2)
-#   if(!missing(TVARobject)){
-#     k<-TVARobject$k
-#     T<-TVARobject$T
-#     p<-TVARobject$lag
-#     modSpe<-TVARobject$model.specific
-#     res<-residuals(TVARobject)
-#     Bmat<-coefMat(TVARobject)
-#     y<-as.matrix(TVARobject$model)[,1:k]
-#     ndig<-getndp(y[,1])
-#     nthresh<-modSpe$nthresh
-#     include <- TVARobject$include
-#     if(nthresh>0){
-#       if(modSpe$oneMatrix)
-#         stop("arg commoninter in TVAR currently not implemented in TVAR.sim")
-#       if(attr(TVARobject, "levelTransVar")=="MTAR")
-#         stop("arg model=MTAR in TVAR currently not implemented in TVAR.sim")
-#       if(attr(TVARobject, "transVar")=="external")
-#         stop("arg thVaR in TVAR currently not implemented in TVAR.sim")
-#       Thresh<-modSpe$Thresh
-#       thDelay<-modSpe$thDelay
-#       combin<-modSpe$transCombin
-#     }
-#   }
-# 
-#   t <- T-p+add 		#Size of end sample
-#   npar<-k*(p+ninc)
-# 
-#   ##### put coefficients vector in right form according to arg include (arg both need no modif)
-#   a<-NULL
-#   if(include=="none")
-#     for(i in 0:nthresh) a<-c(a, i*(p*k)+c(1,2))
-#   else if(include=="const")
-#     for(i in 0:nthresh) a<-c(a, i*(p*k+2)+c(2))
-#   else if(include=="trend")
-#     for(i in 0:nthresh) a<-c(a, i*(p*k+2)+c(1))
-#   #if (include=="both"): correction useless
-#   Bmat<-myInsertCol(Bmat, c=a ,0)
-#   nparBmat<-p*k+2
-#     
-#     
-#   ##############################
-#   ###Reconstitution boot/simul
-#   ##############################
-# 
-#   #initial values
-#   Yb<-matrix(0, nrow=T+add, ncol=k)
-#   Yb[1:p,]<-y[1:p,]		
-# 
-#   if(nthresh>0){
-#     z2<-vector("numeric", length=nrow(y))
-#     z2[1:p]<-y[1:p,]%*%combin
-#   }
-# 
-#   trend<-1:(T+add) #   trend<-c(NA, 1:(T-1+add))
-#   # trend<-c(rep(0, p), trendStart+(0:(T-1))) 
-# 
-#   ##Innovations Specifications 
-#   if(type=="simul"&&dim(innov)!=c(n,k))
-#     stop(paste("input innov is not of right dim, should be matrix with", n,"rows and ", k, "cols\n"))
-#   if(!missing(seed)) set.seed(seed)
-#   innov<-switch(type, "boot"=if(missing(innov)) res[sample(seq_len(t), replace=TRUE),] else innov, "simul"=innov, "check"=res)
-#   resb<-rbind(matrix(0,nrow=p, ncol=k),innov)	
-# 
-#   ## MAIN loop:
-# 
-#   if(nthresh==0){
-# 	  for(i in (p+1):(T+add)){
-# 		  Yb[i,]<-rowSums(cbind(Bmat[,1], Bmat[,2]*trend[i], Bmat[,-c(1,2)]%*%matrix(t(Yb[i-c(1:p),]), ncol=1),resb[i,]))
-# 	  }
-#   } else if(nthresh==1){
-# 	  BDown<-Bmat[,seq_len(nparBmat)]
-# 	  BUp<-Bmat[,-seq_len(nparBmat)]
-# 
-# 	  for(i in (p+1):(T+add)){
-# 	    if(round(z2[i-thDelay],ndig)<=Thresh) {
-# 	      Yb[i,]<-rowSums(cbind(BDown[,1], BDown[,2]*trend[i], BDown[,-c(1,2)]%*%matrix(t(Yb[i-c(1:p),]), ncol=1),resb[i,]))
-# 	      if(round)
-# 	        Yb[i,]<-round(Yb[i,], ndig)
-# 	    }
-# 	    else{
-# 	      Yb[i,]<-rowSums(cbind(BUp[,1], BUp[,2]*trend[i], BUp[,-c(1,2)]%*%matrix(t(Yb[i-c(1:p),]), ncol=1),resb[i,]))
-# 	      if(round)
-# 	        Yb[i,]<-round(Yb[i,], ndig)
-# 	    }
-# 	    z2[i]<-Yb[i,]%*%combin
-# 	  }
-#   }  else if(nthresh==2){
-#     BDown <- Bmat[,seq_len(nparBmat)]
-#     BMiddle <- Bmat[,seq_len(nparBmat)+nparBmat]
-#     BUp <- Bmat[,seq_len(nparBmat)+2*nparBmat]
-#     for(i in (p+1):(T+add)){
-#       if(round(z2[i-thDelay],ndig)<=Thresh[1]) 
-#         Yb[i,]<-rowSums(cbind(BDown[,1], BDown[,2]*trend[i], BDown[,-c(1,2)]%*%matrix(t(Yb[i-c(1:p),]), ncol=1),resb[i,]))
-#       else if(round(z2[i-thDelay],ndig)>Thresh[2]) 
-#         Yb[i,]<-rowSums(cbind(BUp[,1], BUp[,2]*trend[i], BUp[,-c(1,2)]%*%matrix(t(Yb[i-c(1:p),]), ncol=1),resb[i,]))
-#       else
-#         Yb[i,]<-rowSums(cbind(BMiddle[,1],BMiddle[,2]*trend[i], BMiddle[,-c(1,2)]%*%matrix(t(Yb[i-c(1:p),]), ncol=1),resb[i,]))
-#       z2[i]<-Yb[i,]%*%combin
-#     }
-#   }
-#   
-#   
-#   if(FALSE){
-# 	  new<-TVAR_thresh(y=Yb,mTh=mTh,thDelay=thDelay,thVar=thVar, p=p)$trans
-# 	  if(mean(ifelse(new[,thDelay]<Thresh, 1,0))>0.05)
-# 		  list(B=Bmat,Yb=Yb)
-# 	  else
-# 		  Recall(B=Bmat, n=n, lag=lag, nthresh=nthresh, thDelay=thDelay, Thresh, thVar=thVar, mTh=mTh, type=type, starting=starting)
-#   }
-#   # print(cbind(y, round(Yb,3)))
-# 
-#   if(type=="simul"){
-#     Yb <- tail(Yb, -p)
-#     rownames(Yb) <- seq_len(nrow(Yb))
-#   }
-# 
-#   if(show.parMat)
-#     print(Bmat)
-#   res<-if(round) round(Yb, ndig) else Yb
-#   return(res)
-# }
-
-
 
 
 
@@ -369,7 +162,7 @@ TVAR.gen2 <- function(B, n=200, lag=1, include = c("const", "trend","none", "bot
 #'\code{who.parMat=TRUE} will print the matrix as interpreted by the function,
 #'helping the user to feed the right input.
 #'
-#'@aliases TVAR.sim VAR.sim
+#'@aliases TVAR.sim
 #'@param B Matrix of coefficients to simulate
 #'@param Thresh The threshold value(s). Vector of length nthresh
 #'@param nthresh number of threshold (see details)
@@ -381,7 +174,6 @@ TVAR.gen2 <- function(B, n=200, lag=1, include = c("const", "trend","none", "bot
 #'embedding time delay d) PLEASE NOTE that the notation is currently different
 #'to univariate models in tsDyn. The left side variable is taken at time t, and
 #'not t+1 as in univariate cases.
-#'@param thVar external transition variable
 #'@param mTh combination of variables with same lag order for the transition
 #'variable. Either a single value (indicating which variable to take) or a
 #'combination
@@ -393,9 +185,6 @@ TVAR.gen2 <- function(B, n=200, lag=1, include = c("const", "trend","none", "bot
 #'identity matrix.
 #'@param show.parMat Logical. Should the parameter matrix be shown? Useful to
 #'understand how to give right input
-#'@param round Rounds the series created to have the same digits (hopefully) as
-#'original series.
-#'@param seed Optional. Seed for the random number generation.
 #'@param \dots Further arguments passed to the underlying (un-exported)
 #'\code{TVAR.gen} function
 #'@return A matrix with the simulated/bootstraped series.
@@ -406,17 +195,6 @@ TVAR.gen2 <- function(B, n=200, lag=1, include = c("const", "trend","none", "bot
 #'@keywords ts bootstrap
 #'@export
 #'@examples
-#'
-#'##simulate VAR as in Enders 2004, p 268
-#'B1<-matrix(c(0.7, 0.2, 0.2, 0.7), 2)
-#'var1<-TVAR.sim(B=B1,nthresh=0,n=100, include="none")
-#'ts.plot(var1, type="l", col=c(1,2))
-#'
-#'
-#'B2<-rbind(c(0.5, 0.5, 0.5), c(0, 0.5, 0.5))
-#'varcov<-matrix(c(1,0.2, 0.3, 1),2)
-#'var2<-TVAR.sim(B=B2,nthresh=0,n=100, include="const", varcov=varcov)
-#'ts.plot(var2, type="l", col=c(1,2))
 #'
 #'
 #'##Simulation of a TVAR with 1 threshold
@@ -431,11 +209,11 @@ TVAR.gen2 <- function(B, n=200, lag=1, include = c("const", "trend","none", "bot
 #'
 #'
 TVAR.sim <- function(B, Thresh, nthresh=1, n=200, lag=1, include = c("const", "trend","none", "both"),  thDelay=1,  
-                     thVar=NULL, mTh=1, starting=NULL, innov=rmnorm(n, varcov=varcov), 
-                     varcov=diag(1,nrow(B)), show.parMat=FALSE, round=FALSE, seed, ...){
+                     mTh=1, starting=NULL, innov=rmnorm(n, varcov=varcov), 
+                     varcov=diag(1,nrow(B)), show.parMat=FALSE,  ...){
 
-	    TVAR.gen(B=B, Thresh=Thresh, nthresh=nthresh, type="simul", n=n, lag=lag, include = include,  thDelay=thDelay,
-		    thVar=thVar, mTh=mTh, starting=starting, innov=innov, varcov=varcov, show.parMat=show.parMat, round=round, seed=seed, ...)
+	    TVAR.gen(B=B, Thresh=Thresh, nthresh=nthresh, n=n, lag=lag, include = include,  thDelay=thDelay,
+	             mTh=mTh, starting=starting, innov=innov, show.parMat=show.parMat,  ...)
 }
 
 
@@ -452,6 +230,7 @@ TVAR.sim <- function(B, Thresh, nthresh=1, n=200, lag=1, include = c("const", "t
 #'\code{\link{TVAR}}
 #'@param innov Innovations used for bootstrap. If missing, residuals are
 #'resampled.
+#'@param boot.scheme The bootstrap scheme. See details. 
 #'@param seed Optional. Seed for the random resampling function.
 #'@param \dots Further arguments passed to the underlying (un-exported)
 #'\code{TVAR.gen} function
@@ -473,12 +252,12 @@ TVAR.sim <- function(B, Thresh, nthresh=1, n=200, lag=1, include = c("const", "t
 #'TVAR.boot(mod)
 #'
 #'
-TVAR.boot <- function(TVARobject, innov, seed, ...){
-  if(!inherits(TVARobject, "TVAR")) stop("Please provide an object of class 'TVAR' generated by TVAR()")
-  TVAR.gen(TVARobject=TVARobject, type= "boot", innov = innov, seed=seed, ...)
-}
+# TVAR.boot <- function(TVARobject, innov, seed, ...){
+#   if(!inherits(TVARobject, "TVAR")) stop("Please provide an object of class 'TVAR' generated by TVAR()")
+#   TVAR.gen(TVARobject=TVARobject, type= "boot", innov = innov, seed=seed, ...)
+# }
 
-TVAR.boot2 <- function(TVARobject, innov, seed, 
+TVAR.boot <- function(TVARobject, innov, seed, 
                        boot.scheme=c("resample", "wild1", "wild2", "check"), ...){
   if(!inherits(TVARobject, "TVAR")) stop("Please provide an object of class 'TVAR' generated by TVAR()")
   
@@ -503,7 +282,7 @@ TVAR.boot2 <- function(TVARobject, innov, seed,
                   "check"=  resids)
   
   
-  res <- TVAR.gen2(B=TVARobject$coeffmat, n=t, lag=lags, include = TVARobject$include,  
+  res <- TVAR.gen(B=TVARobject$coeffmat, n=t, lag=lags, include = TVARobject$include,  
                    starting=starts, innov=innov,
                    Thresh=getTh(TVARobject), thDelay=TVARobject$model.specific$thDelay,
                    nthresh=TVARobject$model.specific$nthresh,
@@ -514,17 +293,17 @@ TVAR.boot2 <- function(TVARobject, innov, seed,
   res
 }
 
-VAR.sim2 <- function(B,  n=200, lag=1, include = c("const", "trend","none", "both"),  starting=NULL, 
-innov=rmnorm(n,  varcov=varcov), varcov=diag(1,nrow(B)), show.parMat=FALSE, round=FALSE, seed, ...){
-  TVAR.gen(B=B, nthresh=0, type="simul", n=n, lag=lag, include = include,   starting=starting, innov=innov, varcov=varcov, show.parMat=show.parMat, round=round, seed=seed, ...)
-}
-
-VAR.boot2 <- function(VARobject, innov, seed, ...){
-  if(!inherits(VARobject, "VAR")) stop("Please provide an object of class 'VAR' generated by lineVar()")
-  if(attr(VARobject, "varsLevel")!="level") stop("Does not work with VAR in diff or ADf specification")
-
-  TVAR.gen(TVARobject=VARobject,type= "boot", innov = innov, seed=seed, ...)
-}
+# VAR.sim.old <- function(B,  n=200, lag=1, include = c("const", "trend","none", "both"),  starting=NULL,
+#                      innov=rmnorm(n,  varcov=varcov), varcov=diag(1,nrow(B)), show.parMat=FALSE, round=FALSE, seed, ...){
+#   TVAR.gen(B=B, nthresh=0,  n=n, lag=lag, include = include,   starting=starting, innov=innov, varcov=varcov, show.parMat=show.parMat, round=round, seed=seed, ...)
+# }
+# 
+# VAR.boot.old <- function(VARobject, innov, seed, ...){
+#   if(!inherits(VARobject, "VAR")) stop("Please provide an object of class 'VAR' generated by lineVar()")
+#   if(attr(VARobject, "varsLevel")!="level") stop("Does not work with VAR in diff or ADf specification")
+# 
+#   TVAR.gen(TVARobject=VARobject, innov = innov, seed=seed, ...)
+# }
 
 
 if(FALSE){
