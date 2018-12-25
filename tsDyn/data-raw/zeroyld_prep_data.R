d30537318d4a616f674c9300d0b192a38dc35fdd
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2018-12-25 13:23:42 +0100

    data zeroyld: update, add raw datasets

--- tsDyn/data-raw/zeroyld_prep_data.R
+++ tsDyn/data-raw/zeroyld_prep_data.R
@@ -1,14 +1,20 @@
 library(tidyverse)
 library(usethis)
 
-path_orig <-  "/home/matifou/pCloudDrive/Documents/Papers/MyPapers/Articles/Threshold Cointegration Handbook/example_applied/zeroyld1"
 
-path_z <-  "/home/matifou/pCloudDrive/Documents/Papers/MyPapers/Articles/Threshold Cointegration Handbook/example_applied/joe_02r/zeroyld.dat"
-a <- read_csv(path_z)
-dat <- read_table(path_z, col_names = FALSE)
+## local
+setwd("/home/matifou/Dropbox/Documents/tsDyn/tsDyn")
+
+## this file is downloaded from the website. Right headers, not data used by Hansen
+path_orig <-  "data-raw/zeroyld1"
+
+## this one is from hansen. 'Right' data, wrong headers
+path_z <-  "data-raw/zeroyld.dat"
+
+dat <- read_table(path_z, col_names = FALSE) 
 dat_orig <-  read_table(path_orig, skip = 1, col_names = FALSE)
 
-## mats
+## maturities
 dat_orig[1:7,]
 mats <- c(t(as.matrix(dat_orig[1:7,])))
 mats_c <-  mats[!is.na(mats)]
@@ -24,19 +30,31 @@ rs_c
 all(rs_c== mats_c2[1:length(rs_c)] )
 
 colnames(dat) <-  c("Year",  "Month", "NMATS", "NOBS", "SE", "TAX", paste("M", mats_c2, sep="_"))
-dat_fin_old <- dat[, c("M_120", "M_12")]
+dat_c <-  dat %>% 
+  mutate(Year = as.integer(Year),
+         Month = as.integer(Month),
+         Date = as.Date(paste(Year, Month, "01", sep="-"))) %>% 
+  select(Date, Year, Month, everything())
+
+
+dat_fin_old <- dat_c[, c("M_120", "M_12")]
 colnames(dat_fin_old) <-  c("short.run", "long.run")
 
-dat_fin <-  dat[, c("M_12", "M_120")]
+dat_fin <-  dat_c[, c("M_12", "M_120")]
 colnames(dat_fin) <-  c("short.run", "long.run")
 
-## compare with tsDyn
-library(tsDyn)
-data(zeroyld)
-head(zeroyld)
-all.equal(zeroyld, as.data.frame(dat_fin), check.attributes = TRUE)
+#############################
+## compare with tsDyn (old)
+#############################
 
-## hansen code
+# library(tsDyn)
+# data(zeroyld)
+# head(zeroyld)
+# all.equal(zeroyld, as.data.frame(dat_fin), check.attributes = TRUE)
+
+#############################
+## hansen code (old)
+#############################
 dat_2 <- dat[,7:62]
 rs <- rbind(as.matrix(seq(0,18,1)),21,24,30,as.matrix(seq(36,(36+7*12),12)))
 
@@ -51,18 +69,39 @@ dat_new <- dat_2[, c(long_i,short_i)]
 
 dat_new
 
-## prepare
-dat_fin_more <- dat[, c("Year", "Month", "M_12", "M_120")]
-colnames(dat_fin_more) <- c("Year", "Month", "short.run", "long.run")
+## full one
+
+#############################
+## prepare for export
+#############################
+
+## data 2 + time cols
+dat_fin_more <- dat_c[, c("Date", "Year", "Month", "M_12", "M_120")]
+colnames(dat_fin_more) <- c("Date", "Year", "Month", "short.run", "long.run")
 dat_fin_more
 
-head(dat_fin_more)
-tail(dat_fin_more)
+zeroyldMeta <- as.data.frame(dat_fin_more)
+
+## data all
+zeroyldFull <-  dat_c %>% 
+  rename_all(str_to_title) %>% 
+  as.data.frame()
+
+sapply(zeroyldFull, class)
+
+#############################
+
+## export
+#############################
 
 
-## save
-setwd("/home/matifou/Dropbox/Documents/tsDyn/tsDyn/")
-zeroyld_meta <- dat_fin_more
-# use_data(zerolyd_meta)
-# write.csv(dat_fin_more, "/home/matifou/Dropbox/Documents/tsDyn/tsDyn/data/zeroyld_meta.csv")
-write.csv(dat_fin_more, "/home/matifou/Dropbox/Documents/tsDyn/tsDyn/data/zeroyldMeta.csv")
+use_data(zeroyldMeta, overwrite = TRUE)
+# write.csv(dat_fin_more, "/home/matifou/Dropbox/Documents/tsDyn/tsDyn/data/zeroyldMeta.csv")
+# write.csv2(zeroyldMeta, "data/zeroyldMeta.csv",
+#            row.names = FALSE)
+
+
+## test?
+# test_csv <- read.csv("data/zeroyldMeta.csv")
+# test_csv2 <- read.csv2("data/zeroyldMeta.csv")
+# sapply(test_csv2, class)
