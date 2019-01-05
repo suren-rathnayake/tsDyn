016cee7c4a0ab3cb14c874d2fec8cbf7b6f61eaa
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2019-01-05 12:41:59 +0100

    Linear tests: add intercept comparison

--- tsDyn/tests/linear_test.R
+++ tsDyn/tests/linear_test.R
@@ -15,12 +15,28 @@ ar_1_Mean <- ar(lh, order.max =1, demean = TRUE, method = "ols")
 ar_2_noMean <- ar(lh, aic = FALSE, order.max =2, demean = FALSE, method = "ols")
 ar_2_Mean <- ar(lh, aic = FALSE, order.max =2, demean = TRUE, method = "ols")
 
-## compare
+## compare coefs
 all.equal(coef(all_lin[["const_l_1"]])[2], ar_1_Mean$ar[,,1], check.attributes = FALSE)
 all.equal(coef(all_lin[["none_l_1"]]), ar_1_noMean$ar[,,1], check.attributes = FALSE)
 
 all.equal(coef(all_lin[["const_l_2"]])[-1], ar_2_Mean$ar[,,1], check.attributes = FALSE)
 all.equal(coef(all_lin[["none_l_2"]]), ar_2_noMean$ar[,,1], check.attributes = FALSE)
+
+## compare means: intercept in ar is (1-phi)(diff means)
+## diff means is difference between estimated mean, and c/(1- phi)
+linear_1_Mean <-  all_lin[["const_l_1"]] 
+comp_means <- function(ar, linear) {
+  ar_empimean <- ar$x.mean  
+  ar_int <- ar$x.intercept  
+  lin_coefs <- coef(linear)
+  lin_LR_mean <-  ar_mean(linear)
+  diff_means <- lin_LR_mean - ar_empimean
+  res <- c(ar_int = ar_int, diff = (1- sum(lin_coefs[-1])) * diff_means)
+  list(res, check = all.equal(res[1], res[2], check.attributes = FALSE))
+}
+comp_means(ar = ar_1_Mean, linear = linear_1_Mean)$check
+comp_means(ar = ar_2_Mean, linear = all_lin[["const_l_2"]] )$check
+
 
 ## predict?
 all_lin_const <- all_lin[grep("const", names(all_lin))]
