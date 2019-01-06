b7a54fdc9be773abbf2f52ab5d7abc531b512938
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2019-01-06 14:08:19 +0100

    Add templates roxy

---
+++ tsDyn/man-roxygen/param_boot.scheme.R
@@ -0,0 +1,2 @@
+#' @param boot.scheme Type of resampling scheme to use for the residuals. See \code{\link{resample_vec}}.
+#' @md
---
+++ tsDyn/man-roxygen/param_include.R
@@ -0,0 +1,2 @@
+#'@param include Type of deterministic regressors to include: none, a constant, a trend, or constant and trend (\code{both}).
+#' @md
