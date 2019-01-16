291a2b992f68efbdc098c024ffa52234478c3175
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2019-01-15 19:19:52 -0500

    Update test data multivariate

--- tsDyn/data-raw/models_multivariate.R
+++ tsDyn/data-raw/models_multivariate.R
@@ -1,4 +1,5 @@
 library(tsDyn)
+library(vars)
 library(tidyverse)
 
 data(barry)
@@ -7,27 +8,46 @@ data(barry)
 grid_simple <- crossing(lag  =c(1, 2), 
                         include = c("const", "trend", "none", "both"))
 
-## linear
+## VAR, VECM
+var_upd <- function(data, p, type){
+  res <- VAR(data, p = p, type = type)
+  res$call$type <- type
+  res$call$p <- p
+  res
+}
+
 models_VAR <-  grid_simple %>% 
   mutate(model = "VAR",
-         object = map2(lag, include, ~ lineVar(barry, lag =.x, include = .y)))
+         object = map2(lag, include, ~ lineVar(barry, lag =.x, include = .y)),
+         object_vars = map2(lag, include, ~ var_upd(barry, .x, .y)))
 
-models_VECM <-  grid_simple %>% 
+models_VAR$object_vars[[1]]$call
+
+
+models_VECM_tsD <-  grid_simple %>% 
   mutate(model = "VECM",
          object = map2(lag, include, ~ VECM(barry, lag =.x, include = .y)))
 
-## setar
-grid_setar <- crossing(lag  =c(1, 2), 
+models_VECM_vars <- grid_simple %>% 
+  filter(include == "const") %>% 
+  mutate(model = "VECM",
+         object_vars = map2(lag, include, ~ ca.jo(barry, K =.x+1, spec = "transitory")))
+
+models_VECM <- models_VECM_tsD %>% 
+  left_join(models_VECM_vars, by = c("lag", "include", "model"))
+
+## TVAR, TVEC<
+grid_tvar <- crossing(lag  =c(1, 2), 
                        include = c("const", "trend", "none", "both"),
                        nthresh = 1:2)
 
-models_TVAR <-  grid_setar %>% 
+models_TVAR <-  grid_tvar %>% 
   mutate(model = "TVAR",
          object = pmap(list(lag, include, nthresh), 
                        ~suppressWarnings(TVAR(barry, lag =..1, include = ..2, nthresh=..3, 
                                                trace = FALSE))))
 
-models_TVECM <-  grid_setar %>% 
+models_TVECM <-  grid_tvar %>% 
   mutate(model = "TVECM",
          object = pmap(list(lag, include, nthresh), 
                        ~suppressWarnings(TVECM(barry[, 1:2], lag =..1, include = ..2, nthresh=..3, 
